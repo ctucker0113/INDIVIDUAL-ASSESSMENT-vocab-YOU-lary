fa6753ed8bfd33d8c251fbf1b84510be245ae61c
ctucker0113 <134985404+ctucker0113@users.noreply.github.com>
2023-09-07 17:47:15 -0500

    Added navbar with functionality on logout button

--- pages/renderHomePage.js
+++ pages/renderHomePage.js
@@ -1,7 +1,21 @@
 import renderToDom from '../utils/renderToDom';
 
+// Renders Dummy Cards and NavBar to Screen
 const renderHomePage = () => {
-  const homePageHTML = `<div class = "cardBody"> 
+  document.querySelector('#login-form-container').innerHTML = '';
+  const homePageHTML = `
+  <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#" id='homeBtn'>Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#" id='addCardBtn'>Add Card</a>
+  </li>
+  <li class="nav-item">
+  <a class="nav-link" href="#" id='logoutBtn'>Logout</a>
+</li>
+</ul>
+<div class = "cardBody"> 
   <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">Vocab Word 1</h5>
